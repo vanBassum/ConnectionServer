6cdbb7d3092f6924a616cc1566f585d2148ecd69
Bas Visscher <bas.visscher24@gmail.com>
2020-08-24 16:28:33 +0200

    Added IDServer and removed log. Logging should be its own element

--- ConnectionServer/ConnectionServer/ConnectionServer.cs
+++ ConnectionServer/ConnectionServer/ConnectionServer.cs
@@ -11,57 +11,39 @@ namespace ConnectionServer
 {
     public class ConnectionServer
     {
+        readonly IDServer iDServer;
         readonly Router router;
         readonly TcpSocketListener listener;
-        readonly Client client;
+
 
 
         public ConnectionServer()
         {
-            Settings.Save("/data/Settings.json");
-            Logger.SetFile(Settings.LogFile, true);
-            router = new Router(Settings.RouterID);
+            Settings.Load(Settings.defaultSettingsFile, true);
+
+            DummyConnection con_ID_Router = new DummyConnection();
+            DummyConnection con_Router_ID = new DummyConnection();
+            DummyConnection.CoupleConnections(con_ID_Router, con_Router_ID);
+
+            iDServer = new IDServer(con_ID_Router);
+
+            router = new Router();
+            router.AddConnection(con_Router_ID);
             listener = new TcpSocketListener();
             listener.OnClientAccept += Listener_OnClientAccept;
             listener.BeginListening(Settings.ListenerPort);
-            client = new Client(Settings.ClientID);
 
-            DummyConnection con1 = new DummyConnection();
-            DummyConnection con2 = new DummyConnection();
-            DummyConnection.CoupleConnections(con1, con2);
-            router.AddConnection(con1);
-            client.SetConnection(con2);
-            client.OnMessageRecieved += Client_OnMessageRecieved;
-            client.OnBroadcastRecieved += Client_OnBroadcastRecieved;
-        }
-
-        private void Client_OnBroadcastRecieved(object sender, Frame e)
-        {
-            //throw new NotImplementedException();
-        }
-
-        private void Client_OnMessageRecieved(object sender, Frame e)
-        {
-            switch(e.PAY[0])
-            {
-                case 0:
-                    Logger.WriteLine(Regex.Escape(Encoding.ASCII.GetString(e.PAY, 1, e.PAY.Length -1)));
-                    break;
-            }
-            
-            //throw new NotImplementedException();
         }
 
         ~ConnectionServer()
         {
-            Settings.Load("Settings.json");
+            Settings.Save(Settings.defaultSettingsFile);
         }
 
         private void Listener_OnClientAccept(object sender, TcpSocketClient e)
         {
             TCPConnection con = new TCPConnection(e);
             router.AddConnection(con);
-            Logger.WriteLine("Accept connection");
         }
     }
 
--- ConnectionServer/ConnectionServer/Settings.cs
+++ ConnectionServer/ConnectionServer/Settings.cs
@@ -3,11 +3,9 @@ using System;
 
 namespace ConnectionServer
 {
-    public sealed class Settings : BaseSettings
+    public sealed class Settings : BaseSettings<Settings>
     {
         public static int  ListenerPort { get { return GetPar<int>(1000); } set { SetPar(value); } }
-        public static UInt16 RouterID { get { return GetPar<UInt16>(0xFFFF); } set { SetPar(value); } }
-        public static UInt16 ClientID { get { return GetPar<UInt16>(0xFFFE); } set { SetPar(value); } }
         public static string LogFile { get { return GetPar("/data/Log.txt"); } set { SetPar(value); } }
     }
 }
